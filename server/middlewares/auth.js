7ad14c6cc394f9e72293da1f8439328edaa1a504
Soumadip <39thdecember@gmail.com>
2026-08-03 23:15:30 +0530

    fix(auth): Refactored json responses

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -3,12 +3,12 @@ async function protectMw(req, res, next) {
   try {
     const { userId } = await req.auth();
     if (!userId) {
-      return res.json({ success: "false", message: "🔴 Not Authenticated!" });
+      return res.json({ success: false, message: "🔴 Not Authenticated!" });
     }
     next();
   } catch (err) {
     console.log(`ERROR: ${err}`);
-    return res.json({ success: "false", message: err.message });
+    return res.json({ success: false, message: err.message });
   }
 }
 
