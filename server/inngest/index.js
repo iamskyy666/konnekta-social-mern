db72dd7a74458f62ced2e0ba3d4451eec3b11315
Soumadip <39thdecember@gmail.com>
2026-08-03 22:41:31 +0530

    feat(inngest): Add Clerk user sync functions ⚡

---
+++ server/inngest/index.js
@@ -0,0 +1,69 @@
+import { Inngest } from "inngest";
+import UserModel from "../models/user.model.js";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "konnekta-social" });
+
+//! Inngest Function to save user-data to the DB
+const syncUserCreation = inngest.createFunction(
+  {
+    id: "sync-user-from-clerk",
+    triggers: [{ event: "clerk/user.created" }],
+  },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+
+    let username = email_addresses[0].email_address.split("@")[0];
+
+    // Check for username availability
+    const user = await UserModel.findOne({ username });
+
+    if (user) {
+      username += Math.floor(Math.random() * 10000);
+    }
+
+    await UserModel.create({
+      _id: id,
+      email: email_addresses[0].email_address,
+      full_name: `${first_name} ${last_name}`,
+      profile_picture: image_url,
+      username,
+    });
+  },
+);
+
+//! Inngest Fucntion to update user data in DB
+const syncUserUpdation = inngest.createFunction(
+  {
+    id: "update-user-from-clerk",
+    triggers: [{ event: "clerk/user.updated" }],
+  },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+
+    const updateUserData = {
+      email: email_addresses[0].email_address,
+      full_name: `${first_name} ${last_name}`,
+      profile_picture: image_url,
+    };
+
+    await UserModel.findByIdAndUpdate(id, updateUserData);
+  },
+);
+
+//! Inngest Fucntion to delete user data from DB
+const syncUserDeletion = inngest.createFunction(
+  {
+    id: "delete-user-from-clerk",
+    triggers: [{ event: "clerk/user.deleted" }],
+  },
+  async ({ event }) => {
+    const { id } = event.data;
+
+    await UserModel.findByIdAndDelete(id);
+  },
+);
+
+export const functions = [syncUserCreation, syncUserUpdation, syncUserDeletion];
--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./configs/connectDb.js";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js";
 
 const app = express();
 
@@ -11,6 +13,9 @@ app.use(cors());
 app.get("/", (_, res) => {
   res.json({ message: "✅ Konnekta - Server is running!", status: 200 });
 });
+
+// inngest
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 const PORT = process.env.PORT || 4000;
 
