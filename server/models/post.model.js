4b5e543703044a9576fd380ce5c24e2bdd0f409f
Soumadip <39thdecember@gmail.com>
2026-08-07 14:15:42 +0530

    feat(post): add post-model

---
+++ server/models/post.model.js
@@ -0,0 +1,20 @@
+import { Schema, model } from "mongoose";
+
+const postSchema = new Schema(
+  {
+    user: { type: String, ref: "User", required: true },
+    content: { type: String },
+    image_urls: [{ type: String }],
+    post_type: {
+      type: String,
+      enum: ["text", "image", "text_with_image"],
+      required: true,
+    },
+    likes_count: [{ type: String, ref: "User" }],
+  },
+  { timestamps: true, minimize: false },
+);
+
+const PostModel = model("Post", postSchema);
+
+export default PostModel;
