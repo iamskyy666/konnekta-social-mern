62ba2a10411986782b500947bd0afb8db77b4b05
Soumadip <39thdecember@gmail.com>
2026-08-08 15:14:02 +0530

    feat(client): setup initial features/slices in Redux

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { ClerkProvider } from "@clerk/react";
+import { Provider } from "react-redux"; // to use redux everywhere
+import { store } from "./app/store.js";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -14,9 +16,11 @@ if (!PUBLISHABLE_KEY) {
 createRoot(document.getElementById("root")).render(
   <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
     <BrowserRouter>
-      <StrictMode>
-        <App />
-      </StrictMode>
+      <Provider store={store}>
+        <StrictMode>
+          <App />
+        </StrictMode>
+      </Provider>
     </BrowserRouter>
   </ClerkProvider>,
 );
